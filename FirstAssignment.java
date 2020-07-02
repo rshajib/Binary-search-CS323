c20619d317d3c5998f16206a993676af7c05a771
Redowan Shajib <39821409+rshajib@users.noreply.github.com>
2020-07-02 01:20:28 -0400

    Update FirstAssignment.java

--- FirstAssignment.java
+++ FirstAssignment.java
@@ -1,4 +1,4 @@
-
+import java.lang.*;
 import java.util.Formatter ; // import for tabular output
 import java.util.Random;
 public class FirstAssignment {
@@ -13,20 +13,24 @@ public class FirstAssignment {
     	
     	//==========================
     	
-    	AdvancedBinarySearch bs = new AdvancedBinarySearch(); // binary search from lecture 
-    	//BinarySearch bs = new BinarySearch(); // binary search from geekforgeeks, recursive
-    	//BinarySearchLecture bs = new BinarySearchLecture();
+    	//AdvancedBinarySearch bs = new AdvancedBinarySearch();   // Advanced binary search
+    	//BinarySearch bs = new BinarySearch();                 // binary search from geekforgeeks, recursive
+    	
+    	BinarySearchLecture bs = new BinarySearchLecture();   // binary search from lecture
     	
     	//==========================
     	
-        int arr[] = new int[65536]; // initiate array
-        int n = arr.length;  // length of array
-        
+        int arr[] = new int[65536];                            // initiate array
+        int n = arr.length;                                      // length of array
+        int expectedComp = (int) Math.log(n);
+        System.out.println(expectedComp);
+        int leftIndex = 0;
+        int rightIndex = n - 1;
         
         
         for (int i = 0; i<n; i++) 
         {
-             arr[i] = new Random().nextInt(10000000); // assigning random numbers for the array
+             arr[i] = new Random().nextInt(10); // assigning random numbers for the array
         }
         
        QuickSort qs = new QuickSort();
@@ -34,29 +38,40 @@ public class FirstAssignment {
        qs.sort(arr, 0, n-1); // sorting array to run binary search 
         
         
+        String found;
         
+        System.out.println("Trial       Number of Elements        Search Key    Found (Y/N)     Left Index   Right Index      Expected Comp         Actual Comp");
+        	
         
-        for (int i = 1; i <=10; i++)
+        for (int i = 0; i <10; i++)
         {
     
         	 int x = new Random().nextInt(10000000);
-            System.out.println( i + "." + "Random search key: " + x );
+         
         
    
 	
-        int result = bs.AdvancedBinarySearch(arr, 0, n - 1, x); 
+        int result = bs.BinarySearchLecture(arr, 0, n - 1, x); 
         if (result == -1) 
-            System.out.println("Element not present"); 
+            found = "no";
         else 
-            System.out.println("Element found at index " + result); 
+        	found = "yes";
         
              int count = bs.count ; 
               
-              System.out.println("Number of comparisons are " + count); 
+             System.out.println(i+"           " +"   " + n + "                  " + x + "           " + found+"                   "+ leftIndex+"         "+ rightIndex+ "       "+"     "+expectedComp+"                 "+ count);   
         
               bs.setCount(0);
-            //  System.out.format("%32s%10d%16s", x , count , "presnt");
+           
+              
+              
+              
+            
+              
                
 }
-}}
+     
+}
+}
+
 
