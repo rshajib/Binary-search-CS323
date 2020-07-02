fd3b04f1aea5d40449865eaf25606489f0a6c97f
Redowan Shajib <39821409+rshajib@users.noreply.github.com>
2020-07-02 13:17:23 -0400

    Update FirstAssignment.java

--- FirstAssignment.java
+++ FirstAssignment.java
@@ -1,11 +1,11 @@
-import java.lang.*;
+import java.lang.Math;
 import java.util.Formatter ; // import for tabular output
 import java.util.Random;
 public class FirstAssignment {
 
  
 	
-	 // Driver method to test  
+
     public static void main(String args[]) 
     { 
     	
@@ -13,24 +13,23 @@ public class FirstAssignment {
     	
     	//==========================
     	
-    	//AdvancedBinarySearch bs = new AdvancedBinarySearch();   // Advanced binary search
+    	AdvancedBinarySearch bs = new AdvancedBinarySearch();   // Advanced binary search
     	//BinarySearch bs = new BinarySearch();                 // binary search from geekforgeeks, recursive
     	
-    	BinarySearchLecture bs = new BinarySearchLecture();   // binary search from lecture
+    	//BinarySearchLecture bs = new BinarySearchLecture();   // binary search from lecture
     	
     	//==========================
     	
         int arr[] = new int[65536];                            // initiate array
         int n = arr.length;                                      // length of array
-        int expectedComp = (int) Math.log(n);
-        System.out.println(expectedComp);
-        int leftIndex = 0;
-        int rightIndex = n - 1;
+        int expectedComp = (int) ( Math.log(n)/Math.log(2)) + 1;   // expected 1 + log2n
+      
+       
         
         
         for (int i = 0; i<n; i++) 
         {
-             arr[i] = new Random().nextInt(10); // assigning random numbers for the array
+             arr[i] = new Random().nextInt(65536); // assigning random numbers for the array
         }
         
        QuickSort qs = new QuickSort();
@@ -47,11 +46,12 @@ public class FirstAssignment {
         {
     
         	 int x = new Random().nextInt(10000000);
-         
+        	 int leftIndex =    0   ;                 //new Random().nextInt(65536) -1;
+             int rightIndex =   n-1 ;                   //new Random().nextInt(65536) -1;    
         
    
 	
-        int result = bs.BinarySearchLecture(arr, 0, n - 1, x); 
+        int result = bs.AdvancedBinarySearch(arr, 0 , n-1, x); 
         if (result == -1) 
             found = "no";
         else 
@@ -59,7 +59,7 @@ public class FirstAssignment {
         
              int count = bs.count ; 
               
-             System.out.println(i+"           " +"   " + n + "                  " + x + "           " + found+"                   "+ leftIndex+"         "+ rightIndex+ "       "+"     "+expectedComp+"                 "+ count);   
+             System.out.println(i+"           " +"   " + n + "                  " + x + "              " + found+"            "+ leftIndex+"       "+ rightIndex+ "       "+"        "+expectedComp+"                 "+ count);   
         
               bs.setCount(0);
            
@@ -69,9 +69,9 @@ public class FirstAssignment {
             
               
                
-}
+   }
      
-}
+  }
 }
 
 
